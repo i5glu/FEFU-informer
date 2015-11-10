72f1f31588c1434a0014a29d3871d965fcbf1b7c
Ilia <musicguy675@gmail.com>
2015-11-10 18:35:49 +1000

    Initial Commit

---
+++ FEFU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FEFU
+//
+//  Created by Илья on 10.11.15.
+//  Copyright © 2015 FARPOST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FEFU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FEFU
+//
+//  Created by Илья on 10.11.15.
+//  Copyright © 2015 FARPOST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
